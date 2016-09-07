95b32fa7d7f1497e98087f9215cb27ba505e1175
SooLee <duplexa@gmail.com>
2016-09-07 16:02:33 -0400

    corrected typo in usage

--- run_scripts/split_fastq.sh
+++ run_scripts/split_fastq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 display_usage() {
   echo -e ""
-  echo -e "This script run the Split_fastq step of a variant calling pipeline based on bwa-gatk."  
+  echo -e "This script runs the Split_fastq step of a variant calling pipeline based on bwa-gatk."  
   echo -e ""
   echo -e "Usage:"
   echo -e "  $0 fastq_gz project_outdir mate"
